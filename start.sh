e38684827947f50d5e6ac858f4ecd658c5e31422
张长伟 <changwei.zhang@hand-china.com>
2020-04-28 17:37:39 +0800

    编辑脚本额

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ mvn clean install
 
 echo "restart application"
 
-ps -ef | grep myboot | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep eureka | grep -v grep | awk '{print $2}' | xargs kill -9
 
-nohup java -jar -Dspring.application.name=myboot -Dspring.profiles.active=dev target/eureka-0.0.1-SNAPSHOT &
+nohup java -jar -Dspring.application.name=eureka -Dspring.profiles.active=dev target/eureka-0.0.1-SNAPSHOT.jar &
